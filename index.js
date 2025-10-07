efaf7bc642324f15534354ca2e3f4ce0264ab75b
mishra77 <deargklife@gmail.com>
2025-10-07 17:30:00 +0530

    Saved local changes before rebase

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { name } = require("ejs");
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 main().catch(err => console.log(err));
 
